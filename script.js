fc971818e0db3d52a30b9073b348d3a21f3f1f89
mryaxyobek <pubgn9642@gmail.com>
2023-08-16 09:45:38 +0500

    added titles

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const elDarkModeBtn = document.querySelector('#js-dark-mode-btn');
 const elDarkFilter = document.querySelector('.dark-filter');
 
-elDarkModeBtn.addEventListener('click', function(){
+elDarkModeBtn.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
     elDarkFilter.classList.toggle('dark-filter-run');
-})
+});
